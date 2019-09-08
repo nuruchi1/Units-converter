704ed1bc2ca770753967450f845cc4c4dd761321
Nathaly Uruchima <nuruchi1@binghamton.edu>
2019-09-07 23:23:32 -0400

    ...

--- app/src/main/java/com/example/unitsconverter/MainActivity.java
+++ app/src/main/java/com/example/unitsconverter/MainActivity.java
@@ -18,11 +18,11 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.Converter);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
